a5edf82499bf956fc942d39692d18fb368697136
Dak <zawulf18@gmail.com>
2022-11-29 06:41:03 -0700

    Welcome Message and Commands, Events, and Handlers intro.

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -4,7 +4,8 @@ require("dotenv").config()
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -16,6 +17,13 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("Hello!")
     }
+})
+
+// #newcomers channel
+const welcomeChannelId = "1044770353022500956"
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the official Discord of WulfCraft Origins!`)
 })
 
 client.login(process.env.TOKEN)
